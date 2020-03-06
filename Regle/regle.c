579e2ef9e90528ae7b85174c2cb4232fdb1b0e2c
Dursam <48018775+Dursam@users.noreply.github.com>
2020-03-06 11:24:56 +0100

    Fonction de tour joueur avec parametre

--- Regle/regle.c
+++ Regle/regle.c
@@ -1,29 +1,6 @@
-#include <regle.h>
 #include <init.h>
-void debut_partie(void){
+#include <regle.h>
 
-  /* Variables d'initialisation */
-  t_tuile * jeu[N],* j1[J],* j2[J],* j3[J],* j4[J],* pile_j1,* pile_j2,* pile_j3,* pile_j4;
-  init_tuile(jeu);
-
-  /* On iniitialise les joueurs et on leur donne 14 tuiles chacun avec un joueur au hasard qui démarre */
-  creer_joueur(j1,J);
-  creer_joueur(j2,J);
-  creer_joueur(j3,J);
-  creer_joueur(j4,J);
-
-  /* On initialise les 4 piles */
-  pile_j1 = creer_tuile();
-  pile_j2 = creer_tuile();
-  pile_j3 = creer_tuile();
-  pile_j4 = creer_tuile();
-
-  /* On distribue les tuiles du jeu au chevalet des joueurs */
-  distribution_joueur(jeu,j1);
-  distribution_joueur(jeu,j2);
-  distribution_joueur(jeu,j3);
-  distribution_joueur(jeu,j4);
-
-  /* On choisi au hasard celui qui commence la partie en lui donnant une tuile suplementaire */
-  demarrage(jeu,j1,j2,j3,j4);
+void tour_joueur(t_tuile * jeu[N],t_tuile * chevalet[J],t_tuile * pile_gauche,t_tuile * pile_droite){
+  printf("ici\n");
 }
