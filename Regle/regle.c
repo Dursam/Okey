76f0b33fb04a77146b9529da8404854bfa028f05
Dursam <48018775+Dursam@users.noreply.github.com>
2020-03-06 09:49:20 +0100

    Update regle.c

--- Regle/regle.c
+++ Regle/regle.c
@@ -1,1 +1,29 @@
 #include "./regle.h"
+
+void debut_partie(void){
+
+  /* Variables d'initialisation */
+  t_tuile * jeu[N],* j1[J],* j2[J],* j3[J],* j4[J],* pile_J1,* pile_J2,* pile_J3,* pile_J4;
+  init_tuile(jeu);
+
+  /* On iniitialise les joueurs et on leur donne 14 tuiles chacun avec un joueur au hasard qui démarre */
+  creer_joueur(j1,J);
+  creer_joueur(j2,J);
+  creer_joueur(j3,J);
+  creer_joueur(j4,J);
+
+  /* On initialise les 4 piles */
+  pile_j1 = creer_tuile();
+  pile_j2 = creer_tuile();
+  pile_j3 = creer_tuile();
+  pile_j4 = creer_tuile();
+
+  /* On distribue les tuiles du jeu au chevalet des joueurs */
+  distribution_joueur(jeu,j1);
+  distribution_joueur(jeu,j2);
+  distribution_joueur(jeu,j3);
+  distribution_joueur(jeu,j4);
+
+  /* On choisi au hasard celui qui commence la partie en lui donnant une tuile suplementaire */
+  demarrage(jeu,j1,j2,j3,j4);
+}
