8217b66024e84352db311cd472294a9699ee9f51
Dursam <48018775+Dursam@users.noreply.github.com>
2020-03-06 11:21:13 +0100

    Update definitions.h

--- definitions.h
+++ definitions.h
@@ -19,5 +19,4 @@ typedef struct s_element { t_tuile * tuile; struct s_element * suivant;} t_eleme
 
 typedef struct s_pile { t_element * premier ;} t_pile;
 
-
-
+t_tuile * jeu[N],* j1[J],* j2[J],* j3[J],* j4[J],* pile_j1,* pile_j2,* pile_j3,* pile_j4;
