efab79eb39b61e9bbb0256d3f91e6d2c419c882a
Duran <samuel_duran@outlook.fr>
2020-04-22 02:27:07 +0200

    màj fonctions branche principal

--- Menu/menu.c
+++ Menu/menu.c
@@ -1,4 +1,3 @@
-
 /**
  * \file menu_principal.c
  * \brief Menu principal du jeu
@@ -8,10 +7,8 @@
  * Menu principal
  *
  */
+
 #include <menu.h>
-#include <affichage.h>
-#include <fonctions_affichage.h>
-#include <jeu.h>
 
 /**
  * \fn void showMenu()
@@ -22,17 +19,17 @@ void showMenu(){
 	int running; //la variable qui gère le choix du menu
 	char list[][30] = { "Jouer en local", "Jouer en ligne","Charger scores", "Quitter" };
 	running = afficher_menu(list, 4);
-	if(running==0){
 
+	if(running==0){
 		  char list2[][30] = { "Joueur contre IA", "Joueurs contre joueurs" };
 		  running = afficher_menu(list2, 2);
-
-
 	}
+
 	else if(running==1){
 		int running =-1;
 		char list3[][30] = { "Héberger une partie", "Rejoindre une partie" };
 		running = afficher_menu(list3, 2);
+
 		if(running == 0){
 			char list4[][30] = { "2 joueurs et 2 IA", "3 joueurs et 1 IA", "4 joeurs" };
 			running = afficher_menu(list4, 1);
--- Menu/menu.h
+++ Menu/menu.h
@@ -1,2 +1,3 @@
+#include <fonctions_affichage.h>
 
 void showMenu();
