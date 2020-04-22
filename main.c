8afbfa9652c8c68b7daec82302b9c0216ad567cb
Dursam <48018775+Dursam@users.noreply.github.com>
2020-04-22 02:31:47 +0200

    Update main.c
    màj fonctions branche principal

--- main.c
+++ main.c
@@ -1,12 +1,10 @@
 
 
 #include <menu.h>
-#include <affichage.h>
-#include <fonctions_affichage.h>
+#include <jeu.h>
 
 int main(void){
 
-  t_tuile * jeu1;
 	init_affichage();
 	showMenu();
 	return 0;
