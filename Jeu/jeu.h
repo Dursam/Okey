40d51cd584df75832adb8d173285f7955bddf87e
Dursam <48018775+Dursam@users.noreply.github.com>
2020-04-23 21:09:50 +0200

    Update jeu.h
    Ajout de fct debut_partie premier_tour_partie et issue_partie

--- Jeu/jeu.h
+++ Jeu/jeu.h
@@ -4,10 +4,10 @@
 // void rejoindrePartie();
 
 /**
-* \fn void debut_partie(t_tuile * chevalet[N_CHEV], t_pile * pile_J1, t_pile * pile_J2, t_pile * pile_J3, t_pile * pile_J4, t_tuile * okey, int num_deb)
+* \fn void premier_tour_partie(t_tuile * chevalet[N_CHEV], t_pile * pile_J1, t_pile * pile_J2, t_pile * pile_J3, t_pile * pile_J4, t_tuile * okey, int dem)
 * \brief Début de partie en fonction de la tuile de démarrage
 */
-void debut_partie(t_tuile * chevalet[N_CHEV],t_pile * pile_J1,t_pile * pile_J2,t_pile * pile_J3,t_pile * pile_J4,t_tuile * okey,int num_deb);
+void premier_tour_partie(t_tuile * chevalet[N_CHEV],t_pile * pile_J1,t_pile * pile_J2,t_pile * pile_J3,t_pile * pile_J4,t_tuile * okey,int dem);
 
 /**
 * \fn int pioche_vide(t_tuile * jeu[N_T])
@@ -16,21 +16,20 @@ void debut_partie(t_tuile * chevalet[N_CHEV],t_pile * pile_J1,t_pile * pile_J2,t
 int pioche_vide(t_tuile * jeu[N_T]);
 
 /**
+* \fn int debut_partie(t_tuile * joueur1[N_CHEV],t_tuile * joueur2[N_CHEV],t_tuile * joueur3[N_CHEV],t_tuile * joueur4[N_CHEV],t_pile * pile_J1,t_pile * pile_J2,t_pile * pile_J3,t_pile * pile_J4,t_tuile * J1_p1[N_CHEV],t_tuile * J2_p2[N_CHEV],t_tuile * J3_p3[N_CHEV],t_tuile * J4_p4[N_CHEV],t_tuile * okey,int num_joueur)
+* \brief Débute le première tour de la partie en démarrant avec le joueur qui reçoit une 15 ème tuile
+*/
+int debut_partie(t_tuile * joueur1[N_CHEV],t_tuile * joueur2[N_CHEV],t_tuile * joueur3[N_CHEV],t_tuile * joueur4[N_CHEV],t_pile * pile_J1,t_pile * pile_J2,t_pile * pile_J3,t_pile * pile_J4,t_tuile * J1_p1[N_CHEV],t_tuile * J2_p2[N_CHEV],t_tuile * J3_p3[N_CHEV],t_tuile * J4_p4[N_CHEV],t_tuile * okey,int num_joueur);
+
+/**
+* \fn void issue_partie(int issue_joueur)
+* \brief Annonce de l'issue de la partie un vainqueur ou non
+*/
+void issue_partie(int issue_joueur);
+
+// NOTE : Fonction à raccourcir
+/**
 * \fn int partie_en_cours(t_tuile * jeu[N_T], t_tuile * joueur1[N_CHEV], t_tuile * joueur2[N_CHEV], t_tuile * joueur3[N_CHEV], t_tuile * joueur4[N_CHEV], t_tuile * J1_p1[], t_tuile * J2_p2[], t_tuile * J3_p3[], t_tuile * J4_p4[], t_pile * pile_J1, t_pile * pile_J2, t_pile * pile_J3, t_pile * pile_J4, int num_joueur, t_tuile * okey)
 * \brief Permet le déroulement de la partie, après que le première joueur commence. Retourne le numéro de joueur gagnant
 */
-int partie_en_cours(t_tuile * jeu[N_T]
-                                      ,t_tuile * joueur1[N_CHEV],t_tuile * joueur2[N_CHEV],t_tuile * joueur3[N_CHEV],t_tuile * joueur4[N_CHEV]
-                                      ,t_tuile * J1_p1[],t_tuile * J2_p2[],t_tuile * J3_p3[],t_tuile * J4_p4[]
-                                      ,t_pile * pile_J1,t_pile * pile_J2,t_pile * pile_J3,t_pile * pile_J4
-                                      ,int num_joueur
-                                      ,t_tuile * okey
-                                      );
-
-/* A FAIRE */
-
-// Fonction tour_joueur
-
-// Fonction init_partie
-
-// Fonction fin_partie
+int partie_en_cours(t_tuile * jeu[N_T],t_tuile * joueur1[N_CHEV],t_tuile * joueur2[N_CHEV],t_tuile * joueur3[N_CHEV],t_tuile * joueur4[N_CHEV],t_tuile * J1_p1[],t_tuile * J2_p2[],t_tuile * J3_p3[],t_tuile * J4_p4[],t_pile * pile_J1,t_pile * pile_J2,t_pile * pile_J3,t_pile * pile_J4,int num_joueur,t_tuile * okey);
