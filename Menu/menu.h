13e42623411ce0c68efe52b578775b806f8f74a4
Duran <samuel_duran@outlook.fr>
2020-04-23 21:35:20 +0200

    Mode de jeu: joueurs contre joueurs

--- Menu/menu.h
+++ Menu/menu.h
@@ -1,3 +1,14 @@
+#include <jeu.h>
 #include <fonctions_affichage.h>
 
-void showMenu();
+/**
+ * \fn void showMenu(void)
+ * \brief Gestion et affichage du menu principal
+ */
+void showMenu(void);
+
+/**
+* \fn void mode_4_joueurs(void)
+* \brief Lance le mode de jeu avec 4 joueurs en local
+*/
+void mode_local_4_joueurs(void);
--- main.c
+++ main.c
@@ -1,7 +1,4 @@
-
-
 #include <menu.h>
-#include <jeu.h>
 
 int main(void){
 
